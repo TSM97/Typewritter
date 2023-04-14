e28a9d13135d591eb848e22108005ea7b11da6a6
TSM97 <Gtsamadias@hotmail.com>
2023-04-14 17:53:48 +0300

    OOOK BRO

--- src/App.js
+++ src/App.js
@@ -1,107 +1,94 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
-
+import "./App.css";
+import { getRandomMedianCloseTo2000 } from "./utils";
+import React, { useState, useEffect, useRef } from "react";
+import { v4 } from "uuid";
 
 function App() {
-
   // Generate a List of Random words <--
 
-  let randomWords = require('random-words');
-  const [nextWordsArray, setNextWordsArray]= useState([])
+  let randomWords = require("random-words");
+  const [Words, setWords] = useState([]);
+  const [Writing, setWriting] = useState("");
+  const [score, setScore] = useState(0);
+  const [lose, setLose] = useState(0);
+  const [time, setTime] = useState(2);
 
-  const [Words, setWords] = useState([])
-  const [Writing, setWriting] = useState("")
-  const [score, setScore] = useState(0)
-  const [lose, setLose] = useState(0)
-  let rerender = Words.length % 2 === 0;
+  const mounted = useRef(false);
 
   //Word generate divs
   useEffect(() => {
+    if (mounted.current) {
+      setTimeout(() => {
+        const word = randomWords();
+        const key = v4();
+        console.log(word)
+        setWords((prevWords) => [
+          ...prevWords,
+          {
+            text: word,
+            style: {
+              position: "absolute",
+              left: `${Math.floor(
+                Math.random() * (window.innerWidth - 350) + 150
+              )}px`,
+            },
+            className: 'word',
+            key,
+          },
+        ]);
+        setTime(getRandomMedianCloseTo2000());
+      }, time);
+    }
 
-    const interval = setInterval(() => {
-      let word = randomWords();        
-      setWords(prevWords => [...prevWords, word])
-    }, (Math.floor(Math.random() * 2) + 1) * 1000)
-    return () => clearInterval(interval)
-  }, [rerender])
-
-  useEffect(()=>{
-    const nextWordsArray = Words.map((word)=>{
-      return (<div className='word' key={word} style={{ position: 'absolute', left: Math.floor(Math.random() * (window.innerWidth - 500)) + 150  + "px" }}>
-          {word}
-        </div>)
-    })
-    setNextWordsArray(nextWordsArray)
-    console.log(Words)
-
-  },[Words])
-
-  //disappear word when stuck bot
-  useEffect(() => {
-    const intervalDelete = setInterval(() => {
-      const divs = [...document.getElementsByClassName('word')]
-      divs.forEach(word => {
-        const rect = word.getBoundingClientRect();
-        if (rect.bottom >= window.innerHeight - 100) {
-          word.parentNode.removeChild(word)
-          setLose(l => l + 1)
-        }
-      });
-    }, 1000)
-    return () => clearInterval(intervalDelete)
-  }, [])
+    mounted.current = true;
+  }, [time, randomWords]);
 
   //track writing
   useEffect(() => {
-
     const handleKeyDown = (event) => {
-
       //writing
       if (event.keyCode >= 65 && event.keyCode <= 90) {
-        setWriting(prevWriting => prevWriting + event.key);
+        setWriting((prevWriting) => prevWriting + event.key);
       }
 
       //enter key
       if (event.keyCode === 13) {
-        const divs = document.getElementsByClassName('word')
-        for (let i = 0; i < divs.length; i++) {
-          const value = divs[i].textContent
-          if (value === Writing) {
-            if (value.length >= 7) {
-              divs[i].parentNode.removeChild(divs[i]);
-              setScore(s => s + 2)
-            } else {
-              divs[i].parentNode.removeChild(divs[i]);
-              setScore(s => s + 1)
-            }
-            setWriting(() => "")
-            return
-          }
+        setWords((prevWords) =>
+          prevWords.map((word) => word.text === Writing ? { ...word, className: `${word.className} found` } : word)
+        );
+        const matchedWord = Words.find((word) => word.text === Writing)
+        if (matchedWord) {
+          setTimeout(() => {
+            setWords((prevWords) =>
+              prevWords.filter((word) => {
+                console.log(word.text, "---", matchedWord.text)
+                return word.text !== matchedWord.text;
+              })
+            )
+          }, 500)
         }
-        if (Writing !== "") {
-          setLose(l => l + 1)
-        }
-        setWriting(() => "")
+        setWriting(() => "");
       }
 
       //back using backspace
       if (event.keyCode === 8) {
-        setWriting(Writing.slice(0, -1))
+        setWriting((prevWriting) => prevWriting.slice(0, -1));
       }
-    }
-
-    window.addEventListener('keydown', handleKeyDown)
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [Writing])
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [Writing, setWriting, Words]);
 
-  console.log(Writing)
+  function removeWord(word) {
+    setWords((prevWords) => prevWords.filter((_word) => _word !== word));
+    setLose((prevLose) => prevLose + 1)
+  }
 
   return (
-
     <div className="container">
-      <div className='scoreBoard'>
+      <div className="scoreBoard">
         <div>
           <h3> Victory Points</h3>
           <h2>{score}</h2>
@@ -112,9 +99,21 @@ function App() {
         </div>
         <h1>{Writing}</h1>
       </div>
-      {nextWordsArray}
+      {Words.map((word) => {
+        return (
+          <div
+            className={word.className}
+            onAnimationEnd={() => removeWord(word)}
+            key={word.key}
+            style={{ position: word.style.position, left: word.style.left }}
+          >
+            {word.text}
+          </div>
+        );
+      })}
     </div>
   );
 }
 
 export default App;
+
---
+++ src/utils.js
@@ -0,0 +1,12 @@
+export function getRandomMedianCloseTo2000() {
+    const median = 1500;
+    const range = 1000; // Adjust the range to control how close the random numbers will be to the median
+
+    // Generate a random number between -0.5 and 0.5
+    const randomFactor = Math.random() - 0.5;
+
+    // Calculate the random number close to the median
+    const randomNumber = median + (randomFactor * range);
+
+    return randomNumber;
+}
